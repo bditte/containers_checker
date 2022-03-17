5236bf64a99484b1db487cd3ebcfac9cf6ba065b
bditte <bditte@student.42.fr>
2022-03-17 14:13:43 +0100

    first commit

---
+++ include/containers_checker.hpp
@@ -0,0 +1,9 @@
+#ifndef CONTAINERS_CHECKER_HPP
+# define CONTAINERS_CHECKER_HPP
+
+# include <iostream>
+
+# define FT	0
+# define STD 1
+
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+#include "containers_checker.hpp"
+
+int main()
+{
+	std::cout << "here\n";
+	return (0);
+}
